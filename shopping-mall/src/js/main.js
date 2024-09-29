375f3d408dd7e40f9c36440e8143fb3586d7b1dc
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-09-29 16:18:52 +0900

    조건1 - JS의 상품 정보대로 아래처럼 초기 화면이 나와야 한다.

--- shopping-mall/src/js/main.js
+++ shopping-mall/src/js/main.js
@@ -11,3 +11,22 @@ let products = [
   new Product("배꼽살", 4000),
   new Product("중뱃살", 5000),
 ];
+
+// selectbox 찾기
+const $selectBox = document.getElementById("products");
+
+// 조건 1. JS의 상품 정보대로 아래처럼 초기 화면이 나와야 한다.
+function productsSelectBox() {
+  // products 배열 안의 요소들을 하나씩 찾아서 option으로 만들어줘야 한다.
+  products.forEach((product) => {
+    // option 요소 생성
+    const $option = document.createElement("option");
+    $option.value = product.name;
+    $option.textContent = `${product.name} - ${product.price}`;
+
+    // selectbox에 option 추가
+    $selectBox.appendChild($option);
+  });
+}
+
+productsSelectBox();
